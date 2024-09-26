dd0467251cc76acb2aaf1a326e947e150c741e12
Arthur Monteiro <monteiroamelo@gmail.com>
2024-09-26 13:23:23 -0300

    chore

--- MultiEnginesIOS/Pods/Target Support Files/Pods-MultiEnginesIOS-MultiEnginesIOSUITests/Pods-MultiEnginesIOS-MultiEnginesIOSUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MultiEnginesIOS_MultiEnginesIOSUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MultiEnginesIOS_MultiEnginesIOSUITests
-@end
--- MultiEnginesIOS/Pods/Target Support Files/Pods-MultiEnginesIOSTests/Pods-MultiEnginesIOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MultiEnginesIOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_MultiEnginesIOSTests
-@end
